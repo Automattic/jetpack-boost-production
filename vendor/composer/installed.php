d6b1c393e0eadd819f8f9df0dba1ea740d3567dc
thingalon <thingalon@users.noreply.github.com>
2022-10-20 03:23:53 +0000

    Boost/bump version (#26957)
    * Start a new release cycle for boost
    
    * changelog
    
    * Update composer lock
    
    Co-authored-by: Mark George <thingalon@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3286517518

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,7 +1,7 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.5.2-beta',
-        'version' => '1.5.2.0-beta',
+        'pretty_version' => '1.5.3-alpha',
+        'version' => '1.5.3.0-alpha',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -47,8 +47,8 @@
             'dev_requirement' => false,
         ),
         'automattic/jetpack-boost' => array(
-            'pretty_version' => '1.5.2-beta',
-            'version' => '1.5.2.0-beta',
+            'pretty_version' => '1.5.3-alpha',
+            'version' => '1.5.3.0-alpha',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
--- vendor/jetpack-autoloader/class-autoloader-locator.php
+++ vendor/jetpack-autoloader/class-autoloader-locator.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ1_5_2_beta;
+namespace Automattic\Jetpack\Autoloader\jpb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ1_5_3_alpha;
 
  // phpcs:ignore
 
