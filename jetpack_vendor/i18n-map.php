cf3c5b181e1c9e533b8db3458fc0bca7cb1a5fa4
anomiex <anomiex@users.noreply.github.com>
2022-10-20 14:39:49 +0000

    autoloader, composer-plugin: Sort data in generated files (#26929)
    We don't get the packages to process in a consistent order, and the
    order of the data in the associative arrays in composer-plugin's
    `i18n-map.php` and the autoloader's `jetpack_autoloader_classmap.php`
    doesn't matter. So let's sort those arrays so diffs are cleaner.
    
    Note the data for the autoloader files (`jetpack_autoloader_filemap.php`
    and `jetpack_autoloader_psr4.php`) is already sorted when it's
    generated, so we don't have to do anything for those.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3290384280

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -4,41 +4,41 @@ return array(
   'domain' => 'jetpack-boost',
   'type' => 'plugins',
   'packages' => array(
-    'jetpack-config' => array(
-      'path' => 'jetpack_vendor/automattic/jetpack-config',
-      'ver' => '1.11.9999999-dev',
+    'jetpack-admin-ui' => array(
+      'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
+      'ver' => '0.2.9999999-dev',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
       'ver' => '1.17.9999999-dev',
     ),
-    'jetpack-lazy-images' => array(
-      'path' => 'jetpack_vendor/automattic/jetpack-lazy-images',
-      'ver' => '2.1.9999999-dev',
-    ),
-    'jetpack-plugins-installer' => array(
-      'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
-      'ver' => '0.2.9999999-dev',
-    ),
-    'jetpack-admin-ui' => array(
-      'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.2.9999999-dev',
+    'jetpack-config' => array(
+      'path' => 'jetpack_vendor/automattic/jetpack-config',
+      'ver' => '1.11.9999999-dev',
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
       'ver' => '1.45.9999999-dev',
     ),
-    'jetpack-licensing' => array(
-      'path' => 'jetpack_vendor/automattic/jetpack-licensing',
-      'ver' => '1.7.9999999-dev',
-    ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
       'ver' => '2.2.9999999-dev',
     ),
+    'jetpack-lazy-images' => array(
+      'path' => 'jetpack_vendor/automattic/jetpack-lazy-images',
+      'ver' => '2.1.9999999-dev',
+    ),
+    'jetpack-licensing' => array(
+      'path' => 'jetpack_vendor/automattic/jetpack-licensing',
+      'ver' => '1.7.9999999-dev',
+    ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
       'ver' => '2.3.9999999-dev',
+    ),
+    'jetpack-plugins-installer' => array(
+      'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
+      'ver' => '0.2.9999999-dev',
     ),
   ),
 );
