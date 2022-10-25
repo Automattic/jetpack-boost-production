6c9a6230d6002fb35ef2c679f2e5bdf2ef522a9f
bindlegirl <bindlegirl@users.noreply.github.com>
2022-10-25 09:01:51 +0000

    Boost: version bump (#27047)
    Co-authored-by: bindlegirl <1242807+bindlegirl@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3319452265

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,7 +1,7 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.5.3-alpha',
-        'version' => '1.5.3.0-alpha',
+        'pretty_version' => '1.5.3',
+        'version' => '1.5.3.0',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -47,8 +47,8 @@
             'dev_requirement' => false,
         ),
         'automattic/jetpack-boost' => array(
-            'pretty_version' => '1.5.3-alpha',
-            'version' => '1.5.3.0-alpha',
+            'pretty_version' => '1.5.3',
+            'version' => '1.5.3.0',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
