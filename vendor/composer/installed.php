46ddca00421c02e40cb20807fa29d8c548452582
coder-karen <coder-karen@users.noreply.github.com>
2022-11-18 09:04:34 +0000

    JITMS: Adding jp-admin-notices IDs for the Boost plugin (#27327)
    Co-authored-by: Samiff <samiff@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3495456209

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,7 +1,7 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.5.5-alpha',
-        'version' => '1.5.5.0-alpha',
+        'pretty_version' => '1.6.0-alpha',
+        'version' => '1.6.0.0-alpha',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -47,8 +47,8 @@
             'dev_requirement' => false,
         ),
         'automattic/jetpack-boost' => array(
-            'pretty_version' => '1.5.5-alpha',
-            'version' => '1.5.5.0-alpha',
+            'pretty_version' => '1.6.0-alpha',
+            'version' => '1.6.0.0-alpha',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
