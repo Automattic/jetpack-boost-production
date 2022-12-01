07683f3092d709bfeb16e8a7c65d0bd8316d96d3
anomiex <anomiex@users.noreply.github.com>
2022-12-01 21:09:11 +0000

    Update babel monorepo (#27697)
    * Update babel monorepo
    * Bump indirect deps too
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3596423126

--- jetpack_vendor/automattic/jetpack-plugin-deactivation/build/index.asset.php
+++ jetpack_vendor/automattic/jetpack-plugin-deactivation/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '5304395455ce8debe444');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '14099e13345abb7b57ea');
