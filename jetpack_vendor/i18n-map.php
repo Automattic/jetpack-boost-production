568f1bfd23bd35b54d4425e29eba4eccaa57ce73
nunyvega <nunyvega@users.noreply.github.com>
2022-12-02 20:51:48 +0000

    VideoPress: Initialize new plugin cycle (#27741)
    * Add changes to prerelease branch
    
    * Initialize new VideoPress plugin release cycle
    
    * Fix project versions
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3604913726

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.2.14-alpha',
+      'ver' => '0.2.14',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '1.17.28-alpha',
+      'ver' => '1.17.28',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,11 +18,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '1.47.1-alpha',
+      'ver' => '1.47.1',
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '2.2.35-alpha',
+      'ver' => '2.2.35',
     ),
     'jetpack-lazy-images' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-lazy-images',
@@ -30,11 +30,11 @@ return array(
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
-      'ver' => '1.7.13-alpha',
+      'ver' => '1.7.13',
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '2.5.2-alpha',
+      'ver' => '2.5.2',
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
@@ -42,7 +42,7 @@ return array(
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
-      'ver' => '0.2.2-alpha',
+      'ver' => '0.2.2',
     ),
   ),
 );
