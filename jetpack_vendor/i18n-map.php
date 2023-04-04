29fc7443defe28f8d51829c1a9628154e91af5cd
zinigor <zinigor@users.noreply.github.com>
2023-04-04 23:02:36 +0000

    Starting Jetpack 12.1-a.2. (#29927)
    * Jetpack 12.1-a.1 changelog.
    
    * Jetpack mu-wpcom-plugin 1.1.0 changelog.
    
    * STarting a new cycle.
    
    * Fixed up project versions.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4612982304

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.2.18-alpha',
+      'ver' => '0.2.18',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '1.18.0-alpha',
+      'ver' => '1.18.0',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '1.51.6-alpha',
+      'ver' => '1.51.6',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
@@ -26,11 +26,11 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '2.3.5-alpha',
+      'ver' => '2.3.5',
     ),
     'jetpack-lazy-images' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-lazy-images',
-      'ver' => '2.1.35-alpha',
+      'ver' => '2.1.35',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '2.10.0-alpha',
+      'ver' => '2.10.0',
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
