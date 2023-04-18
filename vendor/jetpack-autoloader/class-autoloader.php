16a8fa64f9285f872b55f36a741dd5cd1a352ef4
thingalon <thingalon@users.noreply.github.com>
2023-04-18 04:58:02 +0000

    Prepare for launch
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4728650509

--- vendor/jetpack-autoloader/class-autoloader.php
+++ vendor/jetpack-autoloader/class-autoloader.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ1_8_0_beta;
+namespace Automattic\Jetpack\Autoloader\jpb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ1_8_0;
 
  // phpcs:ignore
 
