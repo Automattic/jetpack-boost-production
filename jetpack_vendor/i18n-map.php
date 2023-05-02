40a61d6bf43b8ca0a7c4827a52de659d3ba49220
anomiex <anomiex@users.noreply.github.com>
2023-05-02 13:59:04 +0000

    Update wordpress monorepo (#30375)
    * Update wordpress monorepo
    * Remove `@types/wordpress__components`
    
      GB switched to including types in the package instead of using a
      `@types/` package.
    
      This also necessitated some code changes, since they're not publishing
      the component props (see WordPress/gutenberg#50157).
    
      Also of note is that their types for `ClipboardButton` are broken, in
      that they didn't declare that it also accepts all `Button`'s props.
      Since that component is deprecated anyway, we just replace use of it
      with the designated replacement.
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4861850947

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '1.18.1',
+      'ver' => '1.18.2-alpha',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '1.51.7',
+      'ver' => '1.51.8-alpha',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
@@ -26,11 +26,11 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '2.3.6',
+      'ver' => '2.3.7-alpha',
     ),
     'jetpack-lazy-images' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-lazy-images',
-      'ver' => '2.1.36',
+      'ver' => '2.1.37-alpha',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
