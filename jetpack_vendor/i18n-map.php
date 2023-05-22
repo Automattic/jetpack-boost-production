8a54f87ae2019663a1ea86d76825dd5e0c03160a
anomiex <anomiex@users.noreply.github.com>
2023-05-22 15:22:32 +0000

    Backport changelogs from Jetpack 12.2-a.11 release and init 12.2-a.12 (#30840)
    * Changelog edits.
    * Init Jetpack 12.2-a.12
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5047356460

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '1.52.0-alpha',
+      'ver' => '1.52.0',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '2.14.0-alpha',
+      'ver' => '2.14.0',
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
