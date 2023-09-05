76ef6c8ba100237265d40c6d594fbb8ada50b700
jeherve <jeherve@users.noreply.github.com>
2023-09-05 13:47:38 +0000

    General: remove WP 6.1 backwards compatibility checks (#32772)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6085230009

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -26,7 +26,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '1.57.2',
+      'ver' => '1.57.3-alpha1693920191',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '3.4.3',
+      'ver' => '3.4.4-alpha1693920191',
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
