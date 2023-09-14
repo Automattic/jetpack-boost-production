9bd91f4607d7fc51a964c5d4d537f4256d8ccf1a
anomiex <anomiex@users.noreply.github.com>
2023-09-14 18:02:12 +0000

    Update dependency wp-coding-standards/wpcs to v3 (#32608)
    * Update dependency wp-coding-standards/wpcs to v3
    * Remove hacks to allow package to update correctly
      And bump `automattic/vipwpcs` too.
    * Update change entry, should be a major version bump
    * Fix codesniffer tests
    * Remove MediaWiki.WhiteSpace.SpaceAfterClosure
      Squiz.Functions.MultiLineFunctionDeclaration catches the same thing and more.
    * Exclude WordPress.Security.EscapeOutput.ExceptionNotEscaped
    * Fix sniffs:
      * PSR2.Classes.ClassDeclaration.CloseBraceAfterBody
      * PSR2.Classes.ClassDeclaration.ExtendsLine
      * PSR2.Classes.ClassDeclaration.ImplementsLine
      * PSR2.Classes.ClassDeclaration.SpaceAfterKeyword
      * PSR2.Classes.ClassDeclaration.SpaceAfterName
      * PSR2.Classes.ClassDeclaration.SpaceBeforeExtends
      * PSR2.Files.EndFileNewline
        (plus a few PSR2.Methods.FunctionClosingBrace)
      * PSR12.Files.FileHeader.IncorrectOrder
        (which seems easily confused)
      * Squiz.Functions.MultiLineFunctionDeclaration
      * Universal.CodeAnalysis.NoEchoSprintf
      * Universal.Namespaces.DisallowCurlyBraceSyntax.Forbidden
      * Universal.UseStatements.NoLeadingBackslash
      * Universal.UseStatements.NoUselessAliases
        (plus some resulting MediaWiki.Classes.UnusedUseStatement.UnnecessaryUse)
      * Universal.WhiteSpace.CommaSpacing
      * WordPress.Security.EscapeOutput.HeredocOutputNotEscaped
      * WordPress.Security.EscapeOutput.OutputNotEscaped
      * WordPress.WP.DeprecatedParameters
      * WordPress.CodeAnalysis.AssignmentInCondition.FoundInWhileCondition → Generic.CodeAnalysis.AssignmentInCondition.FoundInWhileCondition
      * WordPress.WP.CapitalPDangit.Misspelled → .MisspelledInText or .MisspelledInComment
      * Ignore all existing WordPress.Files.FileName.NotHyphenatedLowercase
    * Work around a WPCS bug.
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6188964767

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,35 +6,35 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.2.23-alpha1694689876',
+      'ver' => '0.2.23-alpha1694712909',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '1.18.10',
+      'ver' => '1.18.11-alpha1694712909',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
-      'ver' => '0.1.2',
+      'ver' => '0.1.3-alpha1694712909',
     ),
     'jetpack-boost-speed-score' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-speed-score',
-      'ver' => '0.2.1',
+      'ver' => '0.2.2-alpha1694712909',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
-      'ver' => '1.15.3',
+      'ver' => '1.15.4-alpha1694712909',
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '1.57.4',
+      'ver' => '1.57.5-alpha1694712909',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
-      'ver' => '0.2.4',
+      'ver' => '0.2.5-alpha1694712909',
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '2.3.18',
+      'ver' => '2.3.19-alpha1694712909',
     ),
     'jetpack-lazy-images' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-lazy-images',
@@ -42,15 +42,15 @@ return array(
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
-      'ver' => '1.8.3',
+      'ver' => '1.8.4-alpha1694712909',
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '3.5.0-alpha1694705388',
+      'ver' => '3.5.0-alpha1694712909',
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
-      'ver' => '0.1.5',
+      'ver' => '0.1.6-alpha1694712909',
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
@@ -58,7 +58,7 @@ return array(
     ),
     'jetpack-wp-js-data-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wp-js-data-sync',
-      'ver' => '0.2.2',
+      'ver' => '0.2.3-alpha1694712909',
     ),
   ),
 );
