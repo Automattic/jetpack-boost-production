ba4fbb75ecf7ee835629f8d514412bf672618656
zinigor <zinigor@users.noreply.github.com>
2023-09-19 19:05:48 +0000

    Backport mu-wpcom-plugin 1.6.20, jetpack 12.6 Changes (#33185)
    * Changelog edits.
    
    * Init new cycle
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6239656827

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,47 +6,47 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.2.23-alpha1694712909',
+      'ver' => '0.2.23',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '1.18.11-alpha1694712909',
+      'ver' => '1.18.11',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
-      'ver' => '0.1.3-alpha1694712909',
+      'ver' => '0.1.3',
     ),
     'jetpack-boost-speed-score' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-speed-score',
-      'ver' => '0.2.2-alpha1694729738',
+      'ver' => '0.2.2',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
-      'ver' => '1.15.4-alpha1694712909',
+      'ver' => '1.15.4',
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '1.57.5-alpha1694712909',
+      'ver' => '1.57.5',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
-      'ver' => '0.2.5-alpha1694712909',
+      'ver' => '0.2.5',
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '2.3.19-alpha1694712909',
+      'ver' => '2.3.19',
     ),
     'jetpack-lazy-images' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-lazy-images',
-      'ver' => '2.2.0-alpha1694618390',
+      'ver' => '2.2.0',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
-      'ver' => '1.8.4-alpha1694712909',
+      'ver' => '1.8.4',
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '3.5.0-alpha1694712909',
+      'ver' => '3.5.0',
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
