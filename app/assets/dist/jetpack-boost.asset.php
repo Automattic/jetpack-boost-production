8671f7004507dce5a54d35e2187a0a5af89cf5a5
thingalon <thingalon@users.noreply.github.com>
2024-01-22 02:02:29 +0000

    Boost: Add transitions for critical css advanced recommendations (#35122)
    * Fix not showing all dismissed recommendations at the same time
    
    * add changelog
    
    * Add transition for critical css advanced recommendations
    
    * Add transition to heading section
    
    * Add transition to heading link
    
    * add changelog
    
    * Fix more list spacing issues
    
    * Fix spacing issues
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7605407660

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '5902590ee3fe783427ad');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '5b69fcfc1980f1931431');
