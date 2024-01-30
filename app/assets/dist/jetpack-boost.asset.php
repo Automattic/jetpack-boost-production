2de115717912b9b250f1eae66c6293575783a50a
thingalon <thingalon@users.noreply.github.com>
2024-01-30 05:05:41 +0000

    Boost: Fix UI issues (#35290)
    * Fix regenerate button
    
    * Fix refresh button on speed-scores
    
    * Make all link buttons consistent
    
    * Remove unused import
    
    * Responsive fixes
    
    * Use jetpack component button in ISA recommendations meta
    
    * Fix alignment for ISA summary
    
    * Fix collapsible meta close button
    
    * changelog
    
    * Remove unnecessary spacing
    
    * Remove unused prop
    
    * Fix speed score tooltip alignment
    
    * Update speed score title elements alignment
    
    * Fix Image CDN tooltip alignment
    
    ---------
    
    Co-authored-by: Peter Petrov <peter.petrov89@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7706496531

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '2fae9b8e4298b0210460');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '9cb81045744d69249ac2');
