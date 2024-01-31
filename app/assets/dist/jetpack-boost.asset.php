0f5ade308b9fbb56ae8b3f2a167805ee171c17fa
pyronaur <pyronaur@users.noreply.github.com>
2024-01-31 17:16:51 +0000

    Jetpack Boost: DataSync Client - Enhanced error handling (#35325)
    * Debug error logs
    
    * Check for `datasync_debug` in window to display debug messages
    
    * Add a comment
    
    * Add `DATASYNC_DEBUG` console message
    
    * Throw DataSync error when failed to parse window
    
    * Clean up error messages
    
    * Add 🔄 to DataSync
    
    * Throw more debuggable DataSyncErrors not Error
    
    * Improve the error source description
    
    * Add js-packages/react-data-sync-client changelog entry.
    
    * Fix: Dark styles & Firefox
    
    * Clean up the styles
    
    * Restyle dark & light mode
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7729673764

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '62cb1e070d153c0e8e1b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'c8af1e49cae48854db2c');
