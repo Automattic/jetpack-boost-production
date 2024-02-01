44dff33569114249b9842ea73a2e8d475387412d
thingalon <thingalon@users.noreply.github.com>
2024-02-01 05:34:29 +0000

    Use React.StrictMode (#35330)
    * Use React.StrictMode
    
    * Add plugins/boost changelog entry.
    
    ---------
    
    Co-authored-by: Mark George <thingalon@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7736658907

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,6 +14,7 @@ This is an alpha version! The changes listed here are not final.
 - Super Cache: Add full page caching
 
 ### Changed
+- Jetpack Boost: Wrapped the main component in React.StrictMode
 - Use Blog ID in links to WPCOM instead of site slug.
 
 ### Fixed
--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'c8af1e49cae48854db2c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'd8573f21d881b7f4eae6');
