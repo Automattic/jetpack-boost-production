b934870627528775797f4a5a1975412daf06b01d
thingalon <thingalon@users.noreply.github.com>
2024-02-09 05:28:41 +0000

    Boost: Move ISA related styles to their respective components (part 1) (#35516)
    * Move multi progress styles
    
    * Remove unused hero stylesheet
    
    * Move pagination styles
    
    * Move thumbnail styles
    
    * Move device styles
    
    * Move pill styles
    
    * Move row-title styles
    
    * Move table-row-hover styles
    
    * Move image-size-row styles
    
    * Move (partially) recommendations page styles
    
    * add changelog
    
    * Fix missing styles for multi progress bubbles
    
    * Move tabs styles
    
    * Reorder styles
    
    * Fix multi progress group styles
    
    * Fix image size row mobile styles
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7839945612

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'c913b412d444ea0c8d8e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'd0184af21a5050860b73');
