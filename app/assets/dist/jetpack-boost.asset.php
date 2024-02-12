cf01b3910323fe3d55f8a8f2f2b54ad072eb7cf0
thingalon <thingalon@users.noreply.github.com>
2024-02-12 03:06:54 +0000

    Boost: Fix Image CDN quality controls UI showing for the free version (#35538)
    * Fix isPremium check returning incorrect value
    
    * add changelog
    
    * Update isPremium check
    
    ---------
    
    Co-authored-by: Mark George <thingalon@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7866730541

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '4076188acff12984fbae');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'ead5a0a6004520b9f7f7');
