7cbca8c336797b3953fd7660a32bc76596dad3af
donnchawp <donnchawp@users.noreply.github.com>
2024-02-21 14:49:58 +0000

    Boost: Fix learn-more links for boost cache issues (#35809)
    * Refactor cacheIssuesLink to use getRedirectUrl
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7990620598

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'c4a8fbf303b83017c2f7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'db340828d969f22aa96e');
