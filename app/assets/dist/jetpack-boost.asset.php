21777f3ac64756db627e39ccb3709d9d629f2611
haqadn <haqadn@users.noreply.github.com>
2024-02-23 09:59:24 +0000

    [Boost] Move debug log store to the proper place (#35889)
    * Fix code layout for debug log
    
    * changelog
    
    ---------
    
    Co-authored-by: Mark George <thingalon@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8017468068

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '58d4fce7db9fdeb31504');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '33f3cf12dc24d77c3e89');
