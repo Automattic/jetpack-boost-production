39a461f70adf76d72cb9648c5cd449bb88b792ca
haqadn <haqadn@users.noreply.github.com>
2024-02-23 09:58:46 +0000

    [Boost] Remove type casting (#35888)
    * Remove type casting from new code
    
    * changelog
    
    ---------
    
    Co-authored-by: Mark George <thingalon@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8017465650

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '927d124e5d79c738c458');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '58d4fce7db9fdeb31504');
