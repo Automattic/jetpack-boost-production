acf8fc0567ccbbd0929419b1c599d54d4b251434
dilirity <dilirity@users.noreply.github.com>
2024-02-27 09:44:19 +0000

    Boost: Add tooltip for cache exceptions UI (#35903)
    * Add exceptions tooltip
    
    * add changelog
    
    * Reduce tooltip width
    
    * Update bypass patterns component to show the children it's wrapping
    
    * Make bypass patterns component children optional
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8062495526

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'd026ff443ea8053084a9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '5d2962b0d6ca9ba482d4');
