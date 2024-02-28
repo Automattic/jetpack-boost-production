8fe8d407260ab886562d79050e80a6d7c1a67214
thingalon <thingalon@users.noreply.github.com>
2024-02-28 00:14:38 +0000

    Boost: Fix cache logging label (#35971)
    * Move 'See Logs' links out of logging toggle label
    
    * changelog
    
    * Increase link font size
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8073021997

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '2cc0b18486b3ad084eff');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '5714838e74a4165e56df');
