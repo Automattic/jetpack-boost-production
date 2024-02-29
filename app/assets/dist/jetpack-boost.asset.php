c394ec1424fca0c383b39c25a05595eab7f534b4
thingalon <thingalon@users.noreply.github.com>
2024-02-29 03:25:27 +0000

    Boost: Update cache exceptions UI to indicate if there's an invalid pattern (#35984)
    * Add error if there's a problem with a pattern during input
    
    * Add pattern sanitization before storing to DB
    
    * add changelog
    
    ---------
    
    Co-authored-by: Mark George <thingalon@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8090474252

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '3796001f545f59b0f574');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '94dd3889e01c95371472');
