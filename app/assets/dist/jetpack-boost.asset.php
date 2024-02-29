96dc5efb970dee4a3da7af5edd54c8d4dee87742
dilirity <dilirity@users.noreply.github.com>
2024-02-29 08:51:19 +0000

    [Boost] Fix E2E tests (#36053)
    * Keep mutation keys the same as query keys, so that updates from the mutation apply to the query
    
    * changelog
    
    ---------
    
    Co-authored-by: Mark George <thingalon@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8093223143

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '94dd3889e01c95371472');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '7824bc6e59a2170c5ded');
