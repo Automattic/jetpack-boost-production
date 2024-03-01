a62ad53491d5e6851306e3d11c43537cf3b01747
haqadn <haqadn@users.noreply.github.com>
2024-03-01 11:39:42 +0000

    Boost: Portback changes (#36113)
    * Update versions and changelogs
    
    * Changelog and readme.txt edits.
    
    * Init 3.1.2-alpha
    
    * Update versioning
    
    * Fix readme
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8110629502

--- jetpack_vendor/automattic/jetpack-wp-js-data-sync/CHANGELOG.md
+++ jetpack_vendor/automattic/jetpack-wp-js-data-sync/CHANGELOG.md
@@ -5,12 +5,9 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [0.4.3-alpha] - unreleased
-
-This is an alpha version! The changes listed here are not final.
-
+## [0.4.3] - 2024-03-01
 ### Fixed
-- Improved error handling and response formatting in DataSync client and PHP classes.
+- Improved error handling and response formatting in DataSync client and PHP classes. [#35962]
 
 ## [0.4.2] - 2024-02-22
 ### Changed
@@ -73,7 +70,7 @@ This is an alpha version! The changes listed here are not final.
 ### Fixed
 - Jetpack Boost: Fix assoc_array schema validation. [#29611]
 
-[0.4.3-alpha]: https://github.com/Automattic/jetpack-wp-js-data-sync/compare/v0.4.2...v0.4.3-alpha
+[0.4.3]: https://github.com/Automattic/jetpack-wp-js-data-sync/compare/v0.4.2...v0.4.3
 [0.4.2]: https://github.com/Automattic/jetpack-wp-js-data-sync/compare/v0.4.1...v0.4.2
 [0.4.1]: https://github.com/Automattic/jetpack-wp-js-data-sync/compare/v0.4.0...v0.4.1
 [0.4.0]: https://github.com/Automattic/jetpack-wp-js-data-sync/compare/v0.3.0...v0.4.0
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInitb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ3_1_1_alpha
+class ComposerStaticInitb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ3_1_2_alpha
 {
     public static $files = array (
         '3773ef3f09c37da5478d578e32b03a4b' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-assets/actions.php',
@@ -377,10 +377,10 @@ class ComposerStaticInitb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ3_1_1_a
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInitb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ3_1_1_alpha::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInitb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ3_1_1_alpha::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInitb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ3_1_1_alpha::$prefixesPsr0;
-            $loader->classMap = ComposerStaticInitb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ3_1_1_alpha::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ3_1_2_alpha::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ3_1_2_alpha::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInitb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ3_1_2_alpha::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInitb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ3_1_2_alpha::$classMap;
 
         }, null, ClassLoader::class);
     }
