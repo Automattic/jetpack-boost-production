4e7cc133598a34a6b59c0989a3f331b9530afaef
thingalon <thingalon@users.noreply.github.com>
2024-03-01 04:02:18 +0000

    Boost: User-friendly errors (#36048)
    * Make boost cache enabling errors user friendly
    
    * changelog
    
    ---------
    
    Co-authored-by: Peter Petrov <peter.petrov89@gmail.com>
    Co-authored-by: Donncha Ó Caoimh <5656673+donnchawp@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8106329434

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '94dd3889e01c95371472');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'c91dc78460b145b422ce');
