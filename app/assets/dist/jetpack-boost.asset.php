ec95af5d8728d7415c6e63a6d54210036ab6f3c1
dilirity <dilirity@users.noreply.github.com>
2024-03-07 10:36:40 +0000

    Boost: Add tracking events for cache related features (#36216)
    * Add tracking events for interacting with bypass patterns and logging
    
    * Add tracking events for cache setup failures
    
    * Remove unnecessary event
    
    * add changelog
    
    * Remove duplicate tracking event
    
    * Move tracking events to page cache setup instead of rest api callback
    
    * Update tracking events to use the same naming structure
    
    * Update cache bypass patterns to track total patterns instead
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8186577896

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '91007d14cae11a098ac1');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '3213a296560df7bbf4ef');
