5a9bb1cd19e3cc1c6b0cd69462cba03a7af0c9aa
dilirity <dilirity@users.noreply.github.com>
2024-03-11 09:31:07 +0000

    Boost: Update links to documentation pages for Page Cache (#36285)
    * Update links to documentation pages
    
    * add changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8230376833

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '0fb49aa65c5c08d7d309');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '184029b874496d13cd24');
