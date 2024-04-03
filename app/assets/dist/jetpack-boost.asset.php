7ba5175c3ddfff378ecfb4281de5f2a9d17abd75
dilirity <dilirity@users.noreply.github.com>
2024-04-03 13:28:19 +0000

    Boost: Add e2e tests for Image Guide (#36716)
    * Update Boost's CLI to include support for image guide module
    
    * Add e2e tests for image guide
    
    * Add Boost's image guide test configuration to e2e test matrix
    
    * add changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8539295651

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '6881e8690a3b38541550');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '433caaa36eb2aaab2516');
