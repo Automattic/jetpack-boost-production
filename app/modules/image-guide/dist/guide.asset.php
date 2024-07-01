1c19baed47893d4a0b7d9aea00fe9c5d53c3cae4
anomiex <anomiex@users.noreply.github.com>
2024-07-01 16:44:23 +0000

    Replace `eslint-plugin-svelte3` with `eslint-plugin-svelte` (#38097)
    The npm package `eslint-plugin-svelte3` is deprecated in favor of
    `eslint-plugin-svelte`. The updated config appears to also require
    `svelte-eslint-parser`.
    
    Then fix some new lints it complains about:
    * Some variables in image-guide's `Popup.svelte` are no longer used
      (since #31145).
    * There's a new lint complaining about use of `@html` being unsafe.
      Our one use was easy to convert to `{#if}`.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/9747798977
    
    Upstream-Ref: Automattic/jetpack@69ad9a9c44fb66f48ecf1da7699b6cfbc4abd105

--- app/modules/image-guide/dist/guide.asset.php
+++ app/modules/image-guide/dist/guide.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n', 'wp-polyfill'), 'version' => '32ee7a08ac970bdd1e1f');
+<?php return array('dependencies' => array('wp-i18n', 'wp-polyfill'), 'version' => '71e8f71b2f04f691bbef');
