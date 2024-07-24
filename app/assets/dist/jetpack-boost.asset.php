0875e50d8d1f54fb53ccdb6f2700d48afa53d17a
coder-karen <coder-karen@users.noreply.github.com>
2024-07-24 19:28:38 +0000

    React compatibility: createRoot import and usage updates to remain compatible with new versions of React (#38495)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/10082660861
    
    Upstream-Ref: Automattic/jetpack@78b262f1ef87ce1837e680ff17e8acc7435c2bfb

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'cb38d22869eaa96d02f7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'a78502cc8ff63360ca75');
