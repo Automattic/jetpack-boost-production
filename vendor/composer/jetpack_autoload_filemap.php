bc5568718f06393c573175692d7d28e9ebc96dec
gmjuhasz <gmjuhasz@users.noreply.github.com>
2024-09-05 13:56:18 +0000

    Changelog and readme.txt edits. (#39253)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/10721599246
    
    Upstream-Ref: Automattic/jetpack@4e25fd02631780b4da5544280869ee154b95ba7e

--- vendor/composer/jetpack_autoload_filemap.php
+++ vendor/composer/jetpack_autoload_filemap.php
@@ -7,11 +7,11 @@ $baseDir   = dirname($vendorDir);
 
 return array(
 	'3773ef3f09c37da5478d578e32b03a4b' => array(
-		'version' => '2.3.6.0-alpha1725514586',
+		'version' => '2.3.6.0',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-assets/actions.php'
 	),
 	'7372b7fb88a9723cf5b76d456eb0b738' => array(
-		'version' => '3.0.0.0-alpha1725479112',
+		'version' => '3.0.0.0',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/actions.php'
 	),
 );
