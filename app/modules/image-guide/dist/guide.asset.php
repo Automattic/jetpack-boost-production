0650d535eefa48756ea4de7cba8ab011de02f683
d-alleyne <d-alleyne@users.noreply.github.com>
2024-09-06 20:28:56 +0000

    Disable terser conditional optimization (#39279)
    * Disable optimization of conditionals
    
    This prevents optimizations such as converting:
    
    condition ? __( 'true', 'domain' ) : __( 'false', 'domain' );
    
    to
    
    __(o?'true':'false','domain' );
    
    which is an invalid optimization for translatable strings
    
    * changelog
    
    * Update tests now that Terser's conditional optimization has been disabled
    
    * changelog
    
    * Remove documentation about unnecessary workaround
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/10744633519
    
    Upstream-Ref: Automattic/jetpack@a4050bffedc64836cd9a41dfb7b700795bdd6a2b

--- app/modules/image-guide/dist/guide.asset.php
+++ app/modules/image-guide/dist/guide.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n', 'wp-polyfill'), 'version' => 'c39fbed4dbd04836738c');
+<?php return array('dependencies' => array('wp-i18n', 'wp-polyfill'), 'version' => '6c5e0bf092ce749cc7b5');
--- app/modules/optimizations/image-cdn/dist/inline-liar.asset.php
+++ app/modules/optimizations/image-cdn/dist/inline-liar.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '3c27533c9702a622d177');
+<?php return array('dependencies' => array(), 'version' => '9b0abc3c4fa6b05a601e');
