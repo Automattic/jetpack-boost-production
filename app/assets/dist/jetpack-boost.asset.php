668c16b37cee695059da4db5e7c35519404c080a
anomiex <anomiex@users.noreply.github.com>
2024-09-06 20:59:19 +0000

    Revert "Disable terser conditional optimization (#39279)" (#39280)
    This reverts commit a4050bffedc64836cd9a41dfb7b700795bdd6a2b.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/10744981873
    
    Upstream-Ref: Automattic/jetpack@100f517d0f912b7a825e2e6cfad2bd835088b7d5

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '29511ea47d964d5e5cbb');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '9690052fafa41ef01e89');
--- jetpack_vendor/automattic/jetpack-plugin-deactivation/build/index.asset.php
+++ jetpack_vendor/automattic/jetpack-plugin-deactivation/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '108616c84ee44c342c91');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '655c3ff32e3192c47697');
