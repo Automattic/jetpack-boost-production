1438b92bf863b11de52223ced73b6a674af650a8
dilirity <dilirity@users.noreply.github.com>
2024-09-25 13:36:40 +0000

    Boost: show a simplified getting started page if pricing is not available (#39526)
    * Show pricing table, even when pricing is missing
    
    * changelog
    
    * Remove X and update notice language
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11033923862
    
    Upstream-Ref: Automattic/jetpack@87c507e70fca19924c487e7183c36c3b306f5322

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '5e5b6ec8d62168b4e8c3');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '4686e652b7c46b0e812c');
