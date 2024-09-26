62ba6caac2e3e87cb568eed1d0bd2e5b78729ce3
dilirity <dilirity@users.noreply.github.com>
2024-09-26 10:59:00 +0000

    Backport boost 3.5.1 Changes (#39543)
    * Changelog and readme.txt edits.
    
    * Update stable tag to 3.5.1
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11050655479
    
    Upstream-Ref: Automattic/jetpack@a841af8bdd310a445c666409545b77b1e5206fe5

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'automattic/jetpack-boost',
-        'pretty_version' => '3.5.1-alpha',
-        'version' => '3.5.1.0-alpha',
+        'pretty_version' => '3.5.1',
+        'version' => '3.5.1.0',
         'reference' => null,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
@@ -47,8 +47,8 @@
             'dev_requirement' => false,
         ),
         'automattic/jetpack-boost' => array(
-            'pretty_version' => '3.5.1-alpha',
-            'version' => '3.5.1.0-alpha',
+            'pretty_version' => '3.5.1',
+            'version' => '3.5.1.0',
             'reference' => null,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
