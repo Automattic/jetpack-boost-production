68dab1117de5848dc11446ab44da0312e00f1469
IanRamosC <IanRamosC@users.noreply.github.com>
2024-10-29 20:35:53 +0000

    My Jetpack: post experiment clean up (#39928)
    * Chore: post-ab test cleanup
    
    * changelog
    
    * Remove condition for experiment
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11581829886
    
    Upstream-Ref: Automattic/jetpack@cd1dedf3a8b6394893399c6cfc6060c90378e857

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '4.35.17-alpha1730226216',
+      'ver' => '4.36.0-alpha1730233245',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
