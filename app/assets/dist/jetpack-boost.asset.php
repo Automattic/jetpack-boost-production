a3ee07ada3fc2428701b8a3cab5103636f3d5cc3
dilirity <dilirity@users.noreply.github.com>
2024-11-07 08:46:46 +0000

    Boost: Cornerstone pages UI adjustments (#40050)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11719306983
    
    Upstream-Ref: Automattic/jetpack@c65025d3652dcaff3f5e1243db2e7cbe19f881ad

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'ecb7eccd64eb902a95ed');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '561deba00e0d5086008a');
