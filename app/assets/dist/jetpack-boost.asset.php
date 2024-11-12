322a63e5e6ad4f27703e045e3bb589fe3354112c
donnchawp <donnchawp@users.noreply.github.com>
2024-11-12 13:40:40 +0000

    Check path of cornerstone urls as well as domain (#40125)
    * Check path of cornerstone urls as well as domain
    
    * changelog
    
    * Update projects/plugins/boost/changelog/update-jetpack-boost-fix-sub-directory-cornerstone-check
    
    Co-authored-by: Adnan Haque <3737780+haqadn@users.noreply.github.com>
    
    * Make sure the cornerstone url starts with the site url
    
    * Make sure url starts with site url
    
    ---------
    
    Co-authored-by: Adnan Haque <3737780+haqadn@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11798209616
    
    Upstream-Ref: Automattic/jetpack@7843920d1b6851f99817b74d0f46bf16a15a9d83

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '71d29744ffac56a3be5c');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '4750b56c96d68cf8553e');
