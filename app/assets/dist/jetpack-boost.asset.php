c107a3979a48ebae4374ea7c0b8cfe18e3438a92
Initsogar <Initsogar@users.noreply.github.com>
2024-11-13 21:09:41 +0000

    Backup: fix backup pricing placeholder (#40157)
    * Fix backup pricing card loading placeholder
    
    * Changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11825621230
    
    Upstream-Ref: Automattic/jetpack@3eeff5b7ea42234b79e617730dc07f075c256764

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '065a07e32ba75e618b6b');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '1e4d5159b338ebe4066f');
