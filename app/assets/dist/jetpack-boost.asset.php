7251fc1be21ee1f926e93fcc96ad9a24e3987273
haqadn <haqadn@users.noreply.github.com>
2024-11-20 07:01:30 +0000

    Boost: Update the cornerstone pages UI (#40165)
    * Update the cornerstone pages UI so it's a panel.
    
    * changelog
    
    * Add cornerstone scss file
    
    * Minor changes to translators comments
    
    * Add the experimental pill back to cornerstone pages
    
    * No need to filter out blank lines when validating.
    
    It's handled by the backend and an error is shown if more than the
    max number of pages is added.
    
    * Use the UpgradeCTA for the upgrade message
    
    * Move UpgradeCTA below save button
    
    * Add description above the list of pages
    
    * Add cornerstone upgrade CTA tracks event
    
    * Move Upgrade CTA further down
    
    * Make sure the input is validated when default value is used
    
    * Add "Learn More" link to Cornerstone support docs
    
    * Clean up cornerstone pages panel
    
    * Change button text
    
    * Fix link color when disabled
    
    * Use jetpack redirect service for cornerstone pages support page link
    
    * Reorder components
    
    * Update list rows for free users
    
    * Reduce description spacing
    
    * Remove top spacing for cornerstone pages section
    
    * Remove highlight/focus from collapsible panels in the UI
    
    * Fix default pages having trailing slashes
    
    * Update class name
    
    * Add track event for loading default pages
    
    * Add tracking event for learn more link
    
    * Make selector broader
    
    Co-authored-by: Adnan Haque <3737780+haqadn@users.noreply.github.com>
    
    * Add clarifying comment
    
    * Update pill dimensions
    
    ---------
    
    Co-authored-by: Peter Petrov <peter.petrov89@gmail.com>
    Co-authored-by: Adnan Haque <3737780+haqadn@users.noreply.github.com>
    Co-authored-by: Adnan Haque <adnan007.id@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11928179121
    
    Upstream-Ref: Automattic/jetpack@333a9ecd37233073f05884ad50f1c4caf5b36533

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '98be71d8921311191612');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'd044502eb40fb199a3d1');
