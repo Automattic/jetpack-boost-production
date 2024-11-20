f2d5d00ba161324ec784d5fc82b4f7d2c75f864e
donnchawp <donnchawp@users.noreply.github.com>
2024-11-20 10:11:17 +0000

    Update to 3.6.0 beta2
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11931144530
    
    Upstream-Ref: Automattic/jetpack@7abec835ac65b08f50f10304b3c875ece3875e29

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '70024e880b49cd37a829');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '073878fac3c3a17926a9');
