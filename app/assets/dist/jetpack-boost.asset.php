0b8e339e10f23c78d277b9de77136e7891daad81
LiamSarsfield <LiamSarsfield@users.noreply.github.com>
2025-01-30 09:32:49 +0000

    React Data Sync Client: Add ReactQuery dev tools (#41357)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13049146242
    
    Upstream-Ref: Automattic/jetpack@b614623dd6a375034318c0887bd6bdf5c655d398

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '73173a64fae602a68128');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '0b1b3286220ddc7e1171');
