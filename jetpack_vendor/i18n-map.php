3ab7de5152e2b761739064843581393cd097833a
haqadn <haqadn@users.noreply.github.com>
2025-02-12 21:37:36 +0000

    Backport boost 3.9.0-beta Changes (#41769)
    * Changelog and readme.txt edits.
    
    * Update projects/js-packages/react-data-sync-client/CHANGELOG.md
    
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    * Update projects/packages/jitm/CHANGELOG.md
    
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    * Update projects/packages/my-jetpack/CHANGELOG.md
    
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    * Update projects/packages/sync/CHANGELOG.md
    
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13295110702
    
    Upstream-Ref: Automattic/jetpack@0a26443c7b58e3d3da41c3464c43df537c0f39b8

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -42,7 +42,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.0.7-alpha1739295229',
+      'ver' => '4.0.7',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.4.4-alpha1739293591',
+      'ver' => '5.4.4',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -58,7 +58,7 @@ return array(
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
-      'ver' => '0.3.3-alpha1737997918',
+      'ver' => '0.3.3',
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
@@ -74,11 +74,11 @@ return array(
     ),
     'jetpack-schema' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-schema',
-      'ver' => '0.2.1-alpha1738349988',
+      'ver' => '0.2.1',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.8.0-alpha1739355574',
+      'ver' => '4.8.0',
     ),
     'jetpack-wp-js-data-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wp-js-data-sync',
