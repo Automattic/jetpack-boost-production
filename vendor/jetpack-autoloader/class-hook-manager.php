4d3ae0cbea16e33cba5e2bc5c9415ebe91fa5d7e
dilirity <dilirity@users.noreply.github.com>
2025-02-17 09:57:43 +0000

    Update versions
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13367471610
    
    Upstream-Ref: Automattic/jetpack@b675bd8c0a3f4dfd8c64f7b83d0d63d36951bda2

--- vendor/jetpack-autoloader/class-hook-manager.php
+++ vendor/jetpack-autoloader/class-hook-manager.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ3_9_1_alpha\al5_0_1;
+namespace Automattic\Jetpack\Autoloader\jpb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ3_9_0\al5_0_1;
 
  // phpcs:ignore
 
