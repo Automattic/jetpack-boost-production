f083a96e3b5e97b93a75c660d1fbeacdc9f5afb3
LiamSarsfield <LiamSarsfield@users.noreply.github.com>
2025-03-06 14:43:00 +0000

    Boost: Update stable tag to 3.10.1 (#42252)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13700948375
    
    Upstream-Ref: Automattic/jetpack@1f1ade47f4e2bcd50921e195dc504fc19c680722

--- vendor/jetpack-autoloader/class-shutdown-handler.php
+++ vendor/jetpack-autoloader/class-shutdown-handler.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ3_10_1\al5_0_2;
+namespace Automattic\Jetpack\Autoloader\jpb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ3_10_2_alpha\al5_0_2;
 
  // phpcs:ignore
 
