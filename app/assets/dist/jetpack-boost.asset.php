d3fb6f7ce28d9b408a9799d7a0a1fb6fefc65d89
dilirity <dilirity@users.noreply.github.com>
2025-03-06 10:07:21 +0000

    Boost: Disable 404 tester on Atomic/WoA sites (#42225)
    * Add spacing above notice when exceptions is expanded
    
    * Disable 404 tester on atomic
    
    * add changelog
    
    * Refactor disabling of 404 tester
    
    * Flip 404 tester check so it makes more sense
    
    * Fix underlined links in ISA summary
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13695862960
    
    Upstream-Ref: Automattic/jetpack@8338d71e1a9d534e95f40079fa899406956792b7

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '32be63bc381eaa8cdf0e');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '5331664445cbfa4f7355');
