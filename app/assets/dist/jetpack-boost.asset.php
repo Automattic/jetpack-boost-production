4141fe9f7e71a7d979c59ab678ad990d731360c2
haqadn <haqadn@users.noreply.github.com>
2025-03-14 15:44:38 +0000

    Update pill style (#42460)
    * Update pill style
    
    * changelog
    
    ---------
    
    Co-authored-by: Peter Petrov <peter.petrov89@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13859929674
    
    Upstream-Ref: Automattic/jetpack@8618ffb66306c77a3eab7e94fbfbf04f6648001c

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '143244243e25dde125e3');
+<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'c34c56055ca990618927');
