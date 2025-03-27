ea6c061e7ae2278ff6d9306224c4de18c0af128a
LiamSarsfield <LiamSarsfield@users.noreply.github.com>
2025-03-27 14:11:13 +0000

    Update version to 3.12.1 and add changelog entry for fixed intermittent error during module loading.
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/14108618530
    
    Upstream-Ref: Automattic/jetpack@6cc178647640962b131308a1f637d57e94cbaabb

--- vendor/jetpack-autoloader/class-path-processor.php
+++ vendor/jetpack-autoloader/class-path-processor.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ3_12_0\al5_0_5;
+namespace Automattic\Jetpack\Autoloader\jpb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ3_12_1\al5_0_5;
 
  // phpcs:ignore
 
