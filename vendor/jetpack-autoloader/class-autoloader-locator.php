c8fc9901c6cf96fcbb62c829a5fa38ce013a93a8
haqadn <haqadn@users.noreply.github.com>
2025-05-19 19:55:50 +0000

    Update text for 4.0.0
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/15121926408
    
    Upstream-Ref: Automattic/jetpack@72eb79ffcddb04a2bff5cb12a2d19d7d18ad1ca6

--- vendor/jetpack-autoloader/class-autoloader-locator.php
+++ vendor/jetpack-autoloader/class-autoloader-locator.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ4_0_0_beta\al5_0_7;
+namespace Automattic\Jetpack\Autoloader\jpb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ4_0_0\al5_0_7;
 
  // phpcs:ignore
 
