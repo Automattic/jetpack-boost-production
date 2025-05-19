512f2d61cea56044d2985d6053c9cba646eb4015
LiamSarsfield <LiamSarsfield@users.noreply.github.com>
2025-05-19 12:22:30 +0000

    Boost: Remove beta tag from Auto-resize lazy images (#43496)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/15112491996
    
    Upstream-Ref: Automattic/jetpack@3ea3e609964795668a0ac7a9b7ee54cf6b7dfc7d

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 This is an alpha version! The changes listed here are not final.
 
 ### Changed
+- Auto-resize lazy images: Remove beta tag.
 - My Jetpack: Optimize the images for onboarding slider for faster page load.
 
 ### Removed
--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '44294212537ea5c85b1d');
+<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'f8d0dbdb642786cccd4b');
