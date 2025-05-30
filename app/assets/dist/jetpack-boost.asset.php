75b9a0b4d0731c171109da1a7130f1baa3659947
LiamSarsfield <LiamSarsfield@users.noreply.github.com>
2025-05-30 13:51:23 +0000

    Boost: Surface Error details for LCP Optimization (#43647)
    Co-authored-by: Adnan Haque <adnan007.id@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/15347967765
    
    Upstream-Ref: Automattic/jetpack@3224cfdbe6f0b79eff92f9fbaef05729b4baffc9

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'aa4272c818f2657b2273');
+<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '8130f6c1092ac8b64b48');
