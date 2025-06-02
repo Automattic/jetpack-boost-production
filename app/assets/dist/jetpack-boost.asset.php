ec7806aa4096b98996fc43852dd298ecc5d8df3a
kraftbj <kraftbj@users.noreply.github.com>
2025-06-02 21:31:44 +0000

    Boost: Refresh speed scores after LCP is analyzed (#43494)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/15402972435
    
    Upstream-Ref: Automattic/jetpack@632c7ed0cd13160d39b3157af234fae3ba06b68c

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '6c163bf74fdb68851496');
+<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '53b28cecb01d6549f584');
