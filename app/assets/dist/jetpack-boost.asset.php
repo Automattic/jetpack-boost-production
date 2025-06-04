93077bfa59e723854e052af56b8d0de0ac22039f
LiamSarsfield <LiamSarsfield@users.noreply.github.com>
2025-06-04 09:27:05 +0000

    Boost: Update LCP Module Title and Description (#43745)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/15438355938
    
    Upstream-Ref: Automattic/jetpack@4710f365f039c7657e6eb85e8275802a030286de

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '53b28cecb01d6549f584');
+<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '20f8e067d50d9ec229bd');
