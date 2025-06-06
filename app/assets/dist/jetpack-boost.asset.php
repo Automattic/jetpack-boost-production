426eb4b9831c875a324c9fbbc93f949eb550050a
dilirity <dilirity@users.noreply.github.com>
2025-06-06 08:33:52 +0000

    Boost: Update LCP Error Messaging within Plugin (#43709)
    Co-authored-by: Peter Petrov <peter.petrov89@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/15486058164
    
    Upstream-Ref: Automattic/jetpack@942f85d5c5be66eb0d0cb050494a5c70c2112e15

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'a2c515ef1f6d488130b8');
+<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '53e20d6e61d4bb66d9fd');
