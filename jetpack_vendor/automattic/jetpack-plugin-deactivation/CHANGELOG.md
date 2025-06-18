98d83d9a36d13f47eb42dc620cd432cb869e9630
tbradsha <tbradsha@users.noreply.github.com>
2025-06-18 15:38:16 +0000

    Changelog and readme.txt edits. (#44002)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/15736927987
    
    Upstream-Ref: Automattic/jetpack@36b1e75c66d04ef3686a0ceab7f3745706aa4d4d

--- jetpack_vendor/automattic/jetpack-plugin-deactivation/CHANGELOG.md
+++ jetpack_vendor/automattic/jetpack-plugin-deactivation/CHANGELOG.md
@@ -5,6 +5,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.3.12-alpha] - unreleased
+
+This is an alpha version! The changes listed here are not final.
+
+### Changed
+- Update dependencies.
+
 ## [0.3.11] - 2025-06-04
 ### Changed
 - Update dependencies. [#43569]
@@ -108,6 +115,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 - Added package to intercept plugin deactivation [#27081]
 
+[0.3.12-alpha]: https://github.com/Automattic/jetpack-plugin-deactivation/compare/v0.3.11...v0.3.12-alpha
 [0.3.11]: https://github.com/Automattic/jetpack-plugin-deactivation/compare/v0.3.10...v0.3.11
 [0.3.10]: https://github.com/Automattic/jetpack-plugin-deactivation/compare/v0.3.9...v0.3.10
 [0.3.9]: https://github.com/Automattic/jetpack-plugin-deactivation/compare/v0.3.8...v0.3.9
--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.27-alpha1750107007',
+      'ver' => '4.0.27',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -26,11 +26,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.13.4',
+      'ver' => '6.13.5',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.2.24-alpha1750107007',
+      'ver' => '0.2.24',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
@@ -42,7 +42,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.2.23',
+      'ver' => '4.2.24',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.16.4-alpha1750160746',
+      'ver' => '5.16.4',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -58,7 +58,7 @@ return array(
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
-      'ver' => '0.3.11',
+      'ver' => '0.3.12-alpha1750260074',
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
