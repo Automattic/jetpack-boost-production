c6478c4a2dee6fb31eaca0a3643bc94a70090977
kraftbj <kraftbj@users.noreply.github.com>
2025-07-28 17:20:30 +0000

    Boost: Ensure pending Optimization message appears immediately when enabling LCP (#44496)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/16575383990
    
    Upstream-Ref: Automattic/jetpack@41e8146a1222576f4ffb6fd5b4f2270e1986ba27

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '3155ba0143f52afb76b5');
+<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '29d4c9a887f45d34fa3e');
