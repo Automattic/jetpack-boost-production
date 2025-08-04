5ef5417400350ae2679aaf53988dba6a5bf53bae
gmjuhasz <gmjuhasz@users.noreply.github.com>
2025-08-04 16:36:52 +0000

    Backport jetpack 14.9-beta Changes (#44615)
    * Changelog and readme.txt edits.
    
    * Amend readme.txt
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/16728540644
    
    Upstream-Ref: Automattic/jetpack@7ca4f85a561524b3cbf17e1bbb371b2326463b56

--- vendor/composer/jetpack_autoload_filemap.php
+++ vendor/composer/jetpack_autoload_filemap.php
@@ -7,11 +7,11 @@ $baseDir   = dirname($vendorDir);
 
 return array(
 	'3773ef3f09c37da5478d578e32b03a4b' => array(
-		'version' => '4.3.2.0-alpha1753906593',
+		'version' => '4.3.2.0',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-assets/actions.php'
 	),
 	'7372b7fb88a9723cf5b76d456eb0b738' => array(
-		'version' => '6.17.0.0-alpha1754297358',
+		'version' => '6.17.0.0',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/actions.php'
 	),
 );
