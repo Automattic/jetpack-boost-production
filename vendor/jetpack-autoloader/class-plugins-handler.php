798918c9ff6d59b19f560ba4c19049ee2342efdf
LiamSarsfield <LiamSarsfield@users.noreply.github.com>
2025-08-06 11:19:35 +0000

    Update/boost/backport-4.3.0 (#44655)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/16775098013
    
    Upstream-Ref: Automattic/jetpack@296109c4798923ae1c3a5ce06326bb7d846736f7

--- vendor/jetpack-autoloader/class-plugins-handler.php
+++ vendor/jetpack-autoloader/class-plugins-handler.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ4_3_0_beta\al5_0_9;
+namespace Automattic\Jetpack\Autoloader\jpb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ4_3_0\al5_0_9;
 
  // phpcs:ignore
 
