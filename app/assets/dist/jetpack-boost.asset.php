b8ed4a71cf182ba676927af661bf83c11d587a9e
dilirity <dilirity@users.noreply.github.com>
2025-08-22 14:51:46 +0000

    Boost: Update Critical CSS UI to indicate when generated CSS is too much (#44885)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/17158176526
    
    Upstream-Ref: Automattic/jetpack@e931e09f8925d062a04bdc24da3904b0a7077cea

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '192ff374c4527535ab20');
+<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '2a24fa044049df8a7355');
