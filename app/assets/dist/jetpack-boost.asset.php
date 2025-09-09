904ca381a2818f4ac1222ad92666249a7e6d8968
LiamSarsfield <LiamSarsfield@users.noreply.github.com>
2025-09-09 15:32:49 +0000

    Boost: Fix Cornerstone Pages validation for URLs with GET parameters (#45101)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/17587300550
    
    Upstream-Ref: Automattic/jetpack@4d602586303cad3a6777cd2d75af7b0f63b884b3

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'cbac4429257ad9d43454');
+<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '8925b4c962ad3e5dd8ae');
