8f0860da85fbb43c370db84134f15178ba908669
tbradsha <tbradsha@users.noreply.github.com>
2025-09-11 20:38:15 +0000

    Clean up various common typos in the monorepo (#45148)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/17656336763
    
    Upstream-Ref: Automattic/jetpack@92b9b35201a27203cbbc7109921d81376cd5a6d8

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '8925b4c962ad3e5dd8ae');
+<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '66243ab2b2aebc18b4e8');
--- vendor/jetpack-autoloader/class-latest-autoloader-guard.php
+++ vendor/jetpack-autoloader/class-latest-autoloader-guard.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ4_4_0_alpha\al5_0_9;
+namespace Automattic\Jetpack\Autoloader\jpb1e77e6231d50e7663f84529b6a3dfda_jetpack_boostⓥ4_4_0_alpha\al5_0_10_alpha;
 
  // phpcs:ignore
 
