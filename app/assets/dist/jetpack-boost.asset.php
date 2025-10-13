64c89b302b97642116fa39a1ec2f58ffd7ce2fd5
jeherve <jeherve@users.noreply.github.com>
2025-10-13 15:33:46 +0000

    Woo: update social logo to match recent redesign (#45435)
    * Woo: update social logo to match recent redesign
    
    Fixes #45404
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/18470344726
    
    Upstream-Ref: Automattic/jetpack@26ce8c40395ea5bf5baf3c8d5c69d9d96dfa1b8d

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '807c91b6b9efcdf22175');
+<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'c704ff6b3f042655ea85');
