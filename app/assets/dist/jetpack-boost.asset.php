b9773526174dd4611477014dfed4e6c0f2825461
jeherve <jeherve@users.noreply.github.com>
2025-10-13 16:03:47 +0000

    Social Logos: add new Pocketcasts logo (#45438)
    Fixes #45402
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/18471214488
    
    Upstream-Ref: Automattic/jetpack@87c77236189b404754259aa60253ec2fd73a66aa

--- app/assets/dist/jetpack-boost.asset.php
+++ app/assets/dist/jetpack-boost.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'c704ff6b3f042655ea85');
+<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '3568c1f9e027f62ab6db');
