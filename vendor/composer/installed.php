daec766edc67c0dbf266e9e23e00b6b2814e8314
LiamSarsfield <LiamSarsfield@users.noreply.github.com>
2025-10-15 12:41:07 +0000

    Boost: Backport 4.5.0 Beta changes (#45505)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/18528761659
    
    Upstream-Ref: Automattic/jetpack@3d1dcfa69c26bb3ceb0f8a2934383865433db9ab

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'automattic/jetpack-boost',
-        'pretty_version' => '4.5.0-beta',
-        'version' => '4.5.0.0-beta',
+        'pretty_version' => '4.5.0',
+        'version' => '4.5.0.0',
         'reference' => null,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
@@ -47,8 +47,8 @@
             'dev_requirement' => false,
         ),
         'automattic/jetpack-boost' => array(
-            'pretty_version' => '4.5.0-beta',
-            'version' => '4.5.0.0-beta',
+            'pretty_version' => '4.5.0',
+            'version' => '4.5.0.0',
             'reference' => null,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
