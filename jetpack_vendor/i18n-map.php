4d3eff63fe6fa973ab613c095584734c95c5e257
gmjuhasz <gmjuhasz@users.noreply.github.com>
2026-02-17 17:12:00 +0000

    Backport jetpack 15.6-a.3 changes (#47157)
    * Changelog and readme.txt edits.
    
    * Version bumps
    
    * Update stable tag in readme.txt
    
    * Update lockfiles
    
    * Update lock
    
    * Update lock
    
    ---------
    
    Co-authored-by: Gergely Juhasz <gergely.juhasz@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/22107259469
    
    Upstream-Ref: Automattic/jetpack@2d126a01f3593917afb3a8ab860814f633949a91

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.24-alpha1770735915',
+      'ver' => '4.3.24',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -26,11 +26,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '7.1.0-alpha1770928754',
+      'ver' => '7.1.0',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.4.11-alpha1770928754',
+      'ver' => '0.4.11',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
@@ -42,7 +42,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.21-alpha1770943009',
+      'ver' => '4.3.21',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.31.3-alpha1770988619',
+      'ver' => '5.31.3',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
