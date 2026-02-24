8054c890b0af16d5bd8f3343494354ca8c7bc2d8
anomiex <anomiex@users.noreply.github.com>
2026-02-24 17:18:58 +0000

    Update wordpress monorepo (#47300)
    * Update wordpress monorepo
    * js-packages/components: Button: Loosen ref type
    * js-packages/charts: Update `--wpds-dimension-gap-*` for `@wordpress/theme` 0.7.0.
    * Cleanup obsolete pnpmfile hacks
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/22361230310
    
    Upstream-Ref: Automattic/jetpack@d4d1c32d51d4fddd35007a9ec6d2057040452519

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.25',
+      'ver' => '4.3.26-alpha1771952333',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -26,11 +26,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.0.0',
+      'ver' => '8.0.1-alpha1771952333',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.4.14-alpha1771942982',
+      'ver' => '0.4.14-alpha1771952333',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
@@ -42,7 +42,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.23',
+      'ver' => '4.3.24-alpha1771952333',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.31.7-alpha1771942982',
+      'ver' => '5.31.7-alpha1771952333',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
