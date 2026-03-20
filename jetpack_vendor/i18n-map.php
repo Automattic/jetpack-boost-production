4119cd3b0d6cbba6ea4ae467f29a72814069938f
simison <simison@users.noreply.github.com>
2026-03-20 11:32:59 +0000

    Update wordpress monorepo (#47684)
    * Update wordpress monorepo
    * Remove obsolete patch
    * Bump @types/* and fix various type errors
    * Add eslint-disables for overzealous `@wordpress/no-unknown-ds-tokens` lint
    
    ---------
    
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>
    Co-authored-by: William Viana <vianasw@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/23340310255
    
    Upstream-Ref: Automattic/jetpack@03439481ce6bb7bc3b21069e87bab244198a8f4b

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.28',
+      'ver' => '4.3.29-alpha1774005138',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -26,11 +26,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.0.4',
+      'ver' => '8.0.5-alpha1774005138',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.4.16',
+      'ver' => '0.4.17-alpha1774005138',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
@@ -42,7 +42,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.26',
+      'ver' => '4.3.27-alpha1774005138',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.33.0-alpha1773934239',
+      'ver' => '5.33.0-alpha1774005138',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
