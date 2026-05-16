b33a960c899716f9aa9c8c166a344febd8e216c5
kraftbj <kraftbj@users.noreply.github.com>
2026-05-16 02:02:55 +0000

    Update dependency svelte to v5.55.7 [SECURITY] (#48844)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/25949617893
    
    Upstream-Ref: Automattic/jetpack@8e659a5a2e5c029b383b0b86e695bef3a4884487

--- app/modules/image-guide/dist/guide.asset.php
+++ app/modules/image-guide/dist/guide.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n', 'wp-polyfill'), 'version' => 'd1345ce7434a09c51271');
+<?php return array('dependencies' => array('wp-i18n', 'wp-polyfill'), 'version' => '6c102965235b0a1f6156');
