7534e47dd1b3dafaffb9096169d9a5d6241ceba1
LiamSarsfield <LiamSarsfield@users.noreply.github.com>
2026-06-10 13:48:29 +0000

    Update 4.6.1 Beta to Stable
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/27280570850
    
    Upstream-Ref: Automattic/jetpack@477148eb4044717bc61464eec7b8d7a6d95613d9

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'automattic/jetpack-boost',
-        'pretty_version' => '4.6.1-beta',
-        'version' => '4.6.1.0-beta',
+        'pretty_version' => '4.6.1',
+        'version' => '4.6.1.0',
         'reference' => null,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
@@ -56,8 +56,8 @@
             'dev_requirement' => false,
         ),
         'automattic/jetpack-boost' => array(
-            'pretty_version' => '4.6.1-beta',
-            'version' => '4.6.1.0-beta',
+            'pretty_version' => '4.6.1',
+            'version' => '4.6.1.0',
             'reference' => null,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
