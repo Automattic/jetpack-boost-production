4c60f620ec81c5a027713529cbb84e034a754773
anomiex <anomiex@users.noreply.github.com>
2026-06-22 19:32:41 +0000

    Update dependency webpack to v5.107.2 (#49831)
    * Update dependency webpack to v5.107.2
    * Update indirect `http-proxy-middleware` dep too
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/27977562696
    
    Upstream-Ref: Automattic/jetpack@71faf046c9cc9ab9a7cfb02ef80d3a204ea81c64

--- jetpack_vendor/automattic/jetpack-plugin-deactivation/build/index.asset.php
+++ jetpack_vendor/automattic/jetpack-plugin-deactivation/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '7494c78fcb475f5d996c');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '678ef45f4b2ed0c71169');
--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,15 +6,15 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.9.4',
+      'ver' => '0.9.5-alpha1782155618',
     ),
     'jetpack-agents-manager' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-agents-manager',
-      'ver' => '0.5.1',
+      'ver' => '0.5.2-alpha1782155618',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.4.0',
+      'ver' => '4.4.1-alpha1782155618',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -30,11 +30,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.7.2-alpha1782152687',
+      'ver' => '8.7.2-alpha1782155618',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.4.30',
+      'ver' => '0.4.31-alpha1782155618',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.41',
+      'ver' => '4.3.42-alpha1782155618',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -54,7 +54,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.40.0-alpha1782152687',
+      'ver' => '5.40.0-alpha1782155618',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -62,7 +62,7 @@ return array(
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
-      'ver' => '0.3.29-alpha1781567893',
+      'ver' => '0.3.29-alpha1782155618',
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
