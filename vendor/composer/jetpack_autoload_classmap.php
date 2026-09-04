4dd92263a98e6d56475e0b2419ca11738b14a2b6
retrofox <retrofox@users.noreply.github.com>
2026-09-04 13:34:46 +0000

    Pin the widget dashboard packages to the 202609031004 dev release (#51939)
    * pin widget dashboard packages to a dev release
    
    202609031004, plus dataviews so their types unify
    
    * force one private-apis instance for the dev release
    
    its lock/unlock pair cannot span a stable and a prerelease copy
    
    * collapse prerelease private-apis onto the pin
    rewrites only -next ranges to the version premium-analytics declares; lock regenerated from trunk's
    
    * pin the dashboard column count on read
    widget-dashboard 0.7.1 honours a stored columns preference; placements are authored for four
    
    * sync nested manifests with the dev-release pins
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/33876947486
    
    Upstream-Ref: Automattic/jetpack@2a716f4b624e0ffc25f4ce367772a8f20d84c863

--- vendor/composer/jetpack_autoload_classmap.php
+++ vendor/composer/jetpack_autoload_classmap.php
@@ -999,11 +999,11 @@ return array(
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-wp-abilities/src/class-registrar.php'
 	),
 	'Automattic\\Jetpack\\WP_Build_Polyfills\\WP_Build_Admin_Frame' => array(
-		'version' => '0.5.2.0-alpha1788342643',
+		'version' => '0.5.2.0-alpha1788527654',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-wp-build-polyfills/src/class-wp-build-admin-frame.php'
 	),
 	'Automattic\\Jetpack\\WP_Build_Polyfills\\WP_Build_Polyfills' => array(
-		'version' => '0.5.2.0-alpha1788342643',
+		'version' => '0.5.2.0-alpha1788527654',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-wp-build-polyfills/src/class-wp-build-polyfills.php'
 	),
 	'Automattic\\Jetpack\\WP_JS_Data_Sync\\Contracts\\Data_Sync_Action' => array(
