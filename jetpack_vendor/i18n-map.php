4599e9b4782f2948a4756cda34b2f73303865acb
simison <simison@users.noreply.github.com>
2026-09-10 17:07:40 +0000

    Activity log: use core range calendar component (#52181)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/34504049311
    
    Upstream-Ref: Automattic/jetpack@8af9578eb6afaee68f335f4778f5e9b34907035f

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,7 +6,7 @@ return array(
   'packages' => array(
     'jetpack-activity-log' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-activity-log',
-      'ver' => '0.3.3-alpha1788902387',
+      'ver' => '0.3.3-alpha1789058727',
     ),
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
