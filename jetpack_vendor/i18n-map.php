525c60c51881df55ba57043eb1719c0778c28f33
kraftbj <kraftbj@users.noreply.github.com>
2026-09-18 19:30:45 +0000

    Admin Menu: ignore unsupported sidebar positions, and cover the ordering with tests (#52337)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35384444098
    
    Upstream-Ref: Automattic/jetpack@fd2e3eed6af40e71501be8fd1693b4589a879866

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.12.1-alpha1789670548',
+      'ver' => '0.12.1-alpha1789758629',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
