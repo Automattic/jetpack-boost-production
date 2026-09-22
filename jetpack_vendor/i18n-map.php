c2cc6b3d177d473b2b712b7e84430a9b34b0a088
haqadn <haqadn@users.noreply.github.com>
2026-09-22 17:03:35 +0000

    Backport jetpack 16.3-a.3 changes (#52573)
    * Changelog and readme.txt edits.
    
    * Update stable tag in readme.txt
    
    * Changelog: apply review suggestions for 16.3-a.3
    
    * Changelog: rewrite and split the entries flagged in review
    
    * Changelog: resort entries after the review edits
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35755949971
    
    Upstream-Ref: Automattic/jetpack@b9836a7a8b9ae8d75ffc3ab8dd32c309d209423a

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-activity-log' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-activity-log',
-      'ver' => '0.4.1-alpha1789759296',
+      'ver' => '0.4.1',
     ),
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.13.0-alpha1789762796',
+      'ver' => '0.13.0',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '9.5.0-alpha1790083762',
+      'ver' => '9.6.0-alpha1790095377',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -62,11 +62,11 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '6.5.0-alpha1790063575',
+      'ver' => '6.5.0-alpha1790095377',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.5.2-alpha1789989041',
+      'ver' => '0.5.2',
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
@@ -98,7 +98,7 @@ return array(
     ),
     'jetpack-wp-build-polyfills' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wp-build-polyfills',
-      'ver' => '0.5.7-alpha1789761893',
+      'ver' => '0.5.7',
     ),
     'jetpack-wp-js-data-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wp-js-data-sync',
