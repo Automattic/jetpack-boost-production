909299ba7796f1f04361abdea7fa2aadffb84bed
heydemoura <heydemoura@users.noreply.github.com>
2026-09-24 21:08:30 +0000

    Sync: sync the videopress_playlist_index option (#52728)
    Co-authored-by: Heyde Moura <heyde.moura@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/36056638896
    
    Upstream-Ref: Automattic/jetpack@de14d06ea6b402a3847e5412b991cd722e365cd0

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -90,7 +90,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '5.2.0',
+      'ver' => '5.3.0-alpha1790282525',
     ),
     'jetpack-wp-abilities' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wp-abilities',
